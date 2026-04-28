cf09fb54019d2acf65a4bdc2bcabb7eba5107f45
Pratibha Sharma <pratibhaa625@gmail.com>
2026-04-28 23:19:48 +0545

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,24 @@
 import express from "express";
-import cors from "cors"; 
-import path from "path";
-import dotenv from "dotenv"; 
+import cors from "cors";
+import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js"; 
+import { connectDB } from "./config/db.js";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
-connectDB(); 
-
+connectDB();
 
 app.use(cors({
-    origin: "*",
-    credentials: true
+    origin: "*"
 }));
 
-app.use(express.json()); 
+app.use(express.json());
 
-const PORT = process.env.PORT || 5001; 
-const __dirname = path.resolve();
+app.use("/api/notes", notesRoutes);
 
-app.use("/api/notes",notesRoutes);
+const PORT = process.env.PORT || 5001;
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"));
-    });
-}
-
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port : ${PORT}` );
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
